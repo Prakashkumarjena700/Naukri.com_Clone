8a29eddc485da690b6d5b456e8474d1e948a904e
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2022-11-10 23:41:38 +0530

    added Day3

---
+++ my-app/src/Components/HomeCopmData.jsx
@@ -0,0 +1,29 @@
+import { FcSpeaker, FcDepartment, FcBullish, FcMindMap, FcProcess, FcBusinessman, FcGraduationCap, FcConferenceCall, FcHome, FcServices } from "react-icons/fc";
+
+export const firstFlexData = [
+    { logo: '🏠', name: 'Remote' },
+    { logo: <FcDepartment />, name: 'MNC' },
+    { logo: <FcSpeaker />, name: 'Markrting' },
+    { logo: <FcBullish />, name: 'Analytics' },
+    { logo: <FcMindMap />, name: 'Banking & ..' },
+    { logo: <FcProcess />, name: 'Supply C..' },
+
+]
+export const secondFlexData = [
+    { logo: <FcBusinessman />, name: 'HR' },
+    { logo: <FcGraduationCap />, name: 'Fresher' },
+    { logo: <FcConferenceCall />, name: 'Fortune' },
+    { logo: <FcHome />, name: 'Temp WFH' },
+    { logo: <FcServices />, name: 'Engineering' }
+]
+
+export const TopcompanieshiringnowData=[
+    {name:'MNCs',active:'1.3K+', pics:['https://img.naukimg.com/logo_images/groups/v1/23972.gif','https://img.naukimg.com/logo_images/groups/v1/550568.gif','https://img.naukimg.com/logo_images/groups/v1/4615449.gif','https://img.naukimg.com/logo_images/groups/v1/24254.gif']},
+    {name:'',active:'',pics:[]},
+    {name:'',active:'',pics:[]},
+    {name:'',active:'',pics:[]},
+    {name:'',active:'',pics:[]},
+    {name:'',active:'',pics:[]},
+    {name:'',active:'',pics:[]},
+    {name:'',active:'',pics:[]},
+]
