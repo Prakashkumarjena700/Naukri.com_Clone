16d746cdbd1dec0edd1f57e51465c62020b554ba
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2022-11-11 17:14:35 +0530

    added Day2Update

--- my-app/src/Components/HomeCopmData.jsx
+++ my-app/src/Components/HomeCopmData.jsx
@@ -19,11 +19,14 @@ export const secondFlexData = [
 
 export const TopcompanieshiringnowData=[
     {name:'MNCs',active:'1.3K+', pics:['https://img.naukimg.com/logo_images/groups/v1/23972.gif','https://img.naukimg.com/logo_images/groups/v1/550568.gif','https://img.naukimg.com/logo_images/groups/v1/4615449.gif','https://img.naukimg.com/logo_images/groups/v1/24254.gif']},
-    {name:'',active:'',pics:[]},
-    {name:'',active:'',pics:[]},
-    {name:'',active:'',pics:[]},
-    {name:'',active:'',pics:[]},
-    {name:'',active:'',pics:[]},
-    {name:'',active:'',pics:[]},
-    {name:'',active:'',pics:[]},
+    {name:'Edtech',active:'136',pics:['https://img.naukimg.com/logo_images/groups/v1/4009874.gif','https://img.naukimg.com/logo_images/groups/v1/2657966.gif','https://img.naukimg.com/logo_images/groups/v1/3498410.gif','https://img.naukimg.com/logo_images/groups/v1/1733538.gif']},
+    {name:'Healthcare',active:'122',pics:['https://img.naukimg.com/logo_images/groups/v1/4620817.gif','https://img.naukimg.com/logo_images/groups/v1/643118.gif','https://img.naukimg.com/logo_images/groups/v1/537232.gif','https://img.naukimg.com/logo_images/groups/v1/1422648.gif']},
+    {name:'Unicorns',active:'88',pics:['https://img.naukimg.com/logo_images/groups/v1/4715205.gif','https://img.naukimg.com/logo_images/groups/v1/4301140.gif','https://img.naukimg.com/logo_images/groups/v1/1205138.gif','https://img.naukimg.com/logo_images/groups/v1/366920.gif']},
+    {name:'Internet',active:'139',pics:['https://img.naukimg.com/logo_images/groups/v1/175668.gif','https://img.naukimg.com/logo_images/groups/v1/4404798.gif','https://img.naukimg.com/logo_images/groups/v1/1940648.gif','https://img.naukimg.com/logo_images/groups/v1/257810.gif']},
+    {name:'B2C',active:'930',pics:['https://img.naukimg.com/logo_images/groups/v1/384072.gif','https://img.naukimg.com/logo_images/groups/v1/4657887.gif','https://img.naukimg.com/logo_images/groups/v1/472972.gif','https://img.naukimg.com/logo_images/groups/v1/4592111.gif']},
+    {name:'Manufacturing',active:'168',pics:['https://img.naukimg.com/logo_images/groups/v1/740642.gif','https://img.naukimg.com/logo_images/groups/v1/160432.gif','https://img.naukimg.com/logo_images/groups/v1/4618939.gif','https://img.naukimg.com/logo_images/groups/v1/1939966.gif']},
+    {name:'Fortune 500',active:'78',pics:['https://img.naukimg.com/logo_images/groups/v1/179424.gif','https://img.naukimg.com/logo_images/groups/v1/86854.gif','https://img.naukimg.com/logo_images/groups/v1/141312.gif','https://img.naukimg.com/logo_images/groups/v1/4585477.gif']},
+    {name:'Product',active:'609',pics:['https://img.naukimg.com/logo_images/groups/v1/3712286.gif','https://img.naukimg.com/logo_images/groups/v1/18850.gif','https://img.naukimg.com/logo_images/groups/v1/1477328.gif','https://img.naukimg.com/logo_images/groups/v1/4581857.gif']},
+    {name:'Banking & Finance',active:'142',pics:['https://img.naukimg.com/logo_images/groups/v1/175668.gif','https://img.naukimg.com/logo_images/groups/v1/893890.gif','https://img.naukimg.com/logo_images/groups/v1/454860.gif','https://img.naukimg.com/logo_images/groups/v1/533786.gif']}
 ]
+
--- my-app/src/Components/HomeSlider.jsx
+++ my-app/src/Components/HomeSlider.jsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import './HomeSlider.css'
 import { TopcompanieshiringnowData } from './HomeCopmData'
+import {ChevronRightIcon} from '@chakra-ui/icons'
 
 export const TopcompanieshiringnowSlider = () => {
     const settings = {
@@ -17,8 +18,15 @@ export const TopcompanieshiringnowSlider = () => {
             <Slider {...settings} >
                 {
                     TopcompanieshiringnowData.map((ele) => <div className='Card' >
-                        <div >{ele.name}</div>
-
+                        <p>{ele.name} <ChevronRightIcon/> </p>
+                        <p>{ele.active} are actively hiring</p>
+                        <div className="picContainer" >
+                            {
+                                ele.pics.map((ele) => <div  >
+                                    <img src={ele} alt="🗼" />
+                                </div>)
+                            }
+                        </div>
                     </div>)
                 }
             </Slider>
